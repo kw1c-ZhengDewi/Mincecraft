f1c134ac3daf1923a9bfaae51c863136fa4599e0
Dewi Zheng <dewi.zheng@edu-kw1c.nl>
2025-02-26 13:54:28 +0100

    Test

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>𝕸𝖎𝖓𝖈𝖊𝖈𝖗𝖆𝖋𝖙</title>
+    <title>Mincecraft</title>
 </head>
 
 <body>
     <h1>Welcome to Mincecraft</h1>
 </body>
 
-</html
+</html>

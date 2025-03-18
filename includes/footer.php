63eb7dbc7049c9c69d2ea7138cc0e8dc78c59c01
Dewi Zheng <dewi.zheng@edu-kw1c.nl>
2025-03-18 12:13:23 +0100

    header en navigation saven

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,3 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minecraft Quiz</title>
-    <link rel="stylesheet" href="../styles/stylesheet.css">
-</head>
-<body>
 <footer>
     <div class="footer-container">
         <div class="footer-logos">
@@ -22,9 +12,5 @@
             </div>
         </div>
     </div>
-
 </footer>
-</body>
-</html>
-=======
 

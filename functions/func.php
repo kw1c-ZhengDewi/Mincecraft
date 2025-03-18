db97e9d6a92ee011913b4bad4569d2940523931a
cohor <connor.horstman@edu-kw1c.nl>
2025-03-18 12:16:19 +0100

    .

--- functions/func.php
+++ functions/func.php
@@ -1,1 +1,15 @@
 <?php
+function get_personality($personalityA, $personalityB, $personalityC, $personalityD, $personalityE) {
+    $max_score = max($personalityA, $personalityB, $personalityC, $personalityD, $personalityE);
+    if ($max_score == $personalityA) {
+        return "Steve";
+    } elseif ($max_score == $personalityB) {
+        return "Iron Golem";
+    } elseif ($max_score == $personalityC) {
+        return "Creeper";
+    } elseif ($max_score == $personalityD) {
+        return "Enderman";
+    } else {
+        return "Ender Dragon";
+    }
+}

34219c09a3f202e5bbba41baf3144bf7a12dead7
kw1c-BendersDamian <damian.benders@edu-kw1c.nl>
2025-03-18 14:01:54 +0100

    First commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,30 +1,20 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minecraft Quiz</title>
-    <link rel="stylesheet" href="../styles/stylesheet.css">
-</head>
-<body>
+
 <footer>
     <div class="footer-container">
         <div class="footer-logos">
             <div class="footer-mojang">
-                <img src="../Images/Mojang.png" alt="Mojang Studios logo">
+                <img src="../images/Logo/Mojang_Logo.png" alt="Mojang Studios logo">
             </div>
             <div class="footer-xbox">
-                <img src="../Images/Xbox%20game%20studios.png" alt="Xbox Game Studios logo">
+                <img src="../images/Logo/Logo_Xbox_Game_Studio.png" alt="Xbox Game Studios logo">
             </div>
             <div class="footer-microsoft">
-                <img src="../Images/Microsoft.png" alt="Microsoft logo">
+                <img src="../images/Logo/Logo_%20Microsoft.png" alt="Microsoft logo">
             </div>
         </div>
     </div>
 
 </footer>
-</body>
-</html>
-=======
+
 

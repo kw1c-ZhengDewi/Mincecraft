dd2d0d249d0a6819b6f6a627978928b7316d0da7
cohor <connor.horstman@edu-kw1c.nl>
2025-03-18 11:21:15 +0100

    nieuwe directory geadd voor functions, start formulier page.

--- pages/formulier.php
+++ pages/formulier.php
@@ -1,1 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Authors-->
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mincecraft</title>
+
+    <!-- stylesheet for all pages  -->
+    <link rel="stylesheet" href="../styles/stylesheet.css">
+    <!--  stylesheet only for this page  -->
+    <link rel="stylesheet" href="../styles/formulier.css"
+</head>
+
+<body>
+
+<header>
+    <?php include '../includes/navigation.php'; ?>
+</header>
+
+<?php include '../includes/footer.php'; ?>
+
+</body>
+</html>

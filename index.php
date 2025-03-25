e4ececfbe8a857340de67dd2bdc28fcd7e362590
Dewi Zheng <dewi.zheng@edu-kw1c.nl>
2025-03-25 16:00:05 +0100

    Footer fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- Authors: Dewi, Connor, Damian  -->
+<!--  Authors: Dewi, Connor, Damian  -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

d7cec5d3d4dfa3b67c344324c54e86ba70b299c5
cohor <connor.horstman@edu-kw1c.nl>
2025-04-01 08:58:49 +0200

    evil bug has been fixed

--- functions/func.php
+++ functions/func.php
@@ -1,16 +1,19 @@
 <?php
 function get_personality($personalityA, $personalityB, $personalityC, $personalityD, $personalityE) {
-$personalities = [
-"Steve" => $personalityA,
-"Iron Golem" => $personalityB,
-"Creeper" => $personalityC,
-"Enderman" => $personalityD,
-"Ender Dragon" => $personalityE
-];
-arsort($personalities); // Sort in descending order
-$chosenpersonality = key($personalities); // take highest scoring personality
+    $personalities = [
+        "Steve" => $personalityA,
+        "Iron Golem" => $personalityB,
+        "Creeper" => $personalityC,
+        "Enderman" => $personalityD,
+        "Ender Dragon" => $personalityE
+    ];
 
-setcookie("personality", $chosenpersonality, time() + (86400 * 30), "/");
+    arsort($personalities); // Sort in descending order
+    $chosenperson = key($personalities); // Get highest scorer
 
-    return $chosenpersonality; //return highest scoring personality
+    // Set cookie (valid for 30 days, secure and HTTP-only)
+    //setcookie("personality", "aaa", time() + (86400 * 30), "/", "", true, true);
+    setcookie("personality", "aaa", 10000000, "/", "");
+    return $chosenperson;
 }
+?>
